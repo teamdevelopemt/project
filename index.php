cead80735214671aa0f878adff7541d97d422c65
soramikuu <soramikuu72@gmail.com>
2020-07-05 11:08:25 +0900

    最初の開発ファイル（index.phpとmain.css）の作成

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+//そら
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dare式</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <!-- ダレさん -->
+    <header>
+
+    </header>
+    <main>
+        <!-- ダレさん -->
+        <div class="top"></div>
+        <div class="products"></div>
+        <!-- 内藤さん -->
+        <div class="about"></div>
+        <div class="mentors"></div>
+    </main>
+    <!-- そら -->
+    <footer>
+        <div class="contact"></div>
+        <div class="office"></div>
+    </footer>
+    <!-- 内藤さん -->
+    <script>
+
+    </script>
+</body>
+
+</html>
