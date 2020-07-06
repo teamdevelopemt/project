ddcc48a09100bee708f5422e6b9ecc455f44f986
7itoh <nito007@icloud.com>
2020-07-06 23:32:07 +0900

    サイトのトップページファイル y_naitou 2020/7/6

---
+++ byteen.php
@@ -0,0 +1,200 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>Jumbotron Template · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+  <a class="navbar-brand" href="#">_by Teens</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Top<p>トップページ</p><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Products<p>作品紹介</p></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">About<p>指導方法</p></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Mentors<p>講師紹介</p></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contact<p>お問い合わせ</p></a>
+      </li>
+      <li class="nav-item active mt-3">
+        <a class="nav-link" href="#">Twitter</a>
+      </li>
+      <li class="nav-item active mt-3">
+        <a class="nav-link" href="#">Facebook</a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li> -->
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="キーワード" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
+    </form>
+  </div>
+</nav>
+
+<main role="main">
+
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="jumbotron">
+    <div class="container">
+      <h3 class="display-5 text-center pb-5">10代からはじめるエンジニアリング</h3>
+      <h2 class="display-3 text-center pb-5">_by Teens</h2>
+      <div class="col text-center mt-5">
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Scroll</a></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- PRODUCT -->
+  <div class="container">
+    <!-- Example row of columns -->
+    <h2 class="text-center">PRODUCT</h2>
+    <div class="row text-center">
+      <div class="col-md-4">
+        <img src="" alt="">
+        <p>2020.05.13 入学 14日目</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <img src="" alt="">
+        <p>2020.05.20 入学 21日目</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <img src="" alt="">
+        <p>2020.06.04 入学 35日目</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- ABOUT -->
+  <div class="container">
+    <!-- ABOUT Title -->
+    <h2 class="pt-5 text-center">ABOUT</h2>
+    <!-- row of columns -->
+    <div class="row text-center">
+      <div class="col-md-3">
+        <img src="" alt="">
+        <p>A</p>
+      </div>
+      <div class="col-md-3">
+        <img src="" alt="">
+        <p>B</p>
+      </div>
+      <div class="col-md-3">
+        <img src="" alt="">
+        <p>C</p>
+      </div>
+      <div class="col-md-3">
+        <img src="" alt="">
+        <p>D</p>
+      </div>
+    </div>
+    <div class="text-center">
+      <h3 class="pt-5">教育をデザインする</h3>
+      <div>
+        <p class="pt-2">_by Teens(バイティーンズ)は</p>
+        <p>中高生を対象としたエンジニアリングスクールです。</p>
+        <p>「G's ACADEMY FUKUOKA」に通う現役生が、PCの環境設定からプログラミング、ゲーム作成やHPのUXデザインまで、学習の機会を提供いたします。</p>
+        <p>正解のない人生を自由にデザインし、自信たっぷりの10代を過ごせるようにサポートします。</p>
+      </div>
+    </div>
+
+    <!-- Mentors -->
+    <div class="container pt-3">
+      <!-- row of columns -->
+      <h2 class="text-center">Mentors</h2>
+      <div class="row text-center">
+        <div class="col-md-4">
+          <img src="" alt="">
+          <p>G's ACADEMY FUKUOKA</p>
+          <p>LABコース3期</p>
+          <p>中村 空海</p>
+        </div>
+        <div class="col-md-4">
+          <img src="" alt="">
+          <p>G's ACADEMY FUKUOKA</p>
+          <p>LABコース3期</p>
+          <p>久保 一誠</p>
+        </div>
+        <div class="col-md-4">
+          <img src="" alt="">
+          <p>G's ACADEMY FUKUOKA</p>
+          <p>LABコース3期</p>
+          <p>内藤 洋介</p>
+        </div>
+        <div class="col text-center mt-5">
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">More &raquo;</a></p>
+      </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+  <!-- Office -->
+  <section>
+    <div class="container mt-5">
+      <h2 class="bg-primary text-white office-info-title text-center">Office</h2>
+      <div class="row">
+        <div class="col-md-6 pt-3">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3289.383275152349!2d134.17219535899653!3d34.320614471675356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1594043264815!5m2!1sja!2sjp" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+          </div>
+          
+          <div class="col-md-6 text-left">
+            <h4 class="access-title pt-5 pb-3">_by Teens</h4>
+            <p>〒810-0001 福岡県福岡市中央区天神</p>
+            <p><span>[営業時間]11:00~21:00</span><span>[定休日]水曜日</span></p>
+            <p><span>[TEL]092-***-***</span><span>[email]Issei@dareios.com</span></p>
+          </div>
+      </div>
+  </section>
+
+<footer class="container text-center pt-5">
+  <p>&copy; dareios Company 07/2020</p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.js"></script></body>
+</html>
