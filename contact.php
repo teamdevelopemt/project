5d34f5dd1f8b3438d281d2e66e68af6310c12e22
soramikuu <soramikuu72@gmail.com>
2020-07-07 10:21:36 +0900

    お問い合わせフォームページの作成（contact.php)

---
+++ contact.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html id="SMP_STYLE">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=shift_jis" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <title>Tech Kids School　お問合せフォーム</title>
+  <meta name="robots" content="noindex" />
+
+  <style type="text/css"></style>
+</head>
+
+<body class="body">
+  <div class="body_tbl">
+    <!-- SMP_TEMPLATE_HEADER start -->
+    <h1>Dare式　お問合せフォーム</h1>
+
+    <p class="header_text">以下のフォームに必要な項目を入力のうえ「送信」ボタンを押してください。<br>ご登録いただいたメールアドレスにご連絡をさせていただきます。</p>
+
+    <!-- FORM start -->
+    <form method="post" action="/regist/Reg2">
+      <div class="smp_tmpl">
+        <dl class="cf">
+          <dt class="title">
+            氏名 <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f005316778" value="" maxlength="32">
+            <br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            氏名（カナ） <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f012234715" value="" maxlength="32">
+            <br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            お子様氏名 <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f012234716" value="" maxlength="32">
+            <br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            お子様氏名（カナ）<span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f005316780" value="" maxlength="32">
+            <br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            お子様の学校名・団体名
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f005316782" value="" maxlength="32">
+            <br>
+            <span class="sample">学校、団体に属されていない方は、所属無しとご記入ください。 </span><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+
+        <!-- ラジオボタン、なぜか複数選択できてしまう -->
+        <dl class="cf">
+          <dt class="title">
+            お子様の学年 <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+            <input type="radio" name="first" value="01">高１
+            <input type="radio" name="second" value="02">高２
+            <input type="radio" name="third" value="03">高３
+
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            当HPアクセスのきっかけ <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+            <input type="checkbox" name="first" value="01">検索サイト
+            <input type="checkbox" name="second" value="02">ご紹介
+            <input type="checkbox" name="third" value="03">twitter
+            <input type="checkbox" name="third" value="03">Facebook
+            <input type="checkbox" name="third" value="03">その他
+
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            メールアドレス <span class="need">＊</span>
+            <span class="caution">半角英数字でご入力ください</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f005316783" value="" maxlength="129"><br>
+            （確認用）<br>
+            <input class="input " type="text" name="f005316783:cf" value="" maxlength="129"><br>
+
+            <span class="sample">PCのメールアドレスを推奨しております。</span><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            携帯電話番号 <span class="need">＊</span>
+          </dt>
+          <dd class="data phone">
+            <input class="input " type="text" name="f005316784:a" value="" maxlength="6">
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            お問い合わせ内容 <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+            <input type="checkbox" name="first" value="01">面談希望
+            <input type="checkbox" name="second" value="02">体験希望
+            <input type="checkbox" name="third" value="03">その他
+            <br>
+            <span class="sample">（複数選択可能です。） </span><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            無料面談希望日時
+          </dt>
+          <dd class="data ">
+            <input type="date">
+            <br><br>
+            <input type="checkbox" name="first" value="01">午前
+            <input type="checkbox" name="second" value="02">午後
+            <input type="checkbox" name="third" value="03">夕方以降
+            <br>
+            <span class="sample">（複数選択可能です。） </span><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            無料体験希望日時
+          </dt>
+          <dd class="data ">
+            <input type="date">
+            <br><br>
+            <input type="checkbox" name="first" value="01">午前
+            <input type="checkbox" name="second" value="02">午後
+            <input type="checkbox" name="third" value="03">夕方以降
+            <br>
+            <span class="sample">（複数選択可能です。） </span><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            お問合せ詳細
+          </dt>
+          <dd class="data ">
+            <textarea class="" name="f012234632" rows="4" wrap="soft"></textarea><br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+        <dl class="cf">
+          <dt class="title">
+            氏名 <span class="need">＊</span>
+          </dt>
+          <dd class="data ">
+
+            <input class="input " type="text" name="f005316778" value="" maxlength="32">
+            <br>
+            <span class="msg"></span>
+          </dd>
+        </dl>
+      </div>
+      <input type="hidden" name="detect" value="判定">
+      <!-- HIDDEN START -->
+      <input type="hidden" name="SMPFORM" value="lgke-obsjt-3c793296d9a768ffa8c895846bd64e39">
+      <input type="hidden" name="mode" value="0">
+      <!-- HIDDEN END -->
+      <input class="submit" type="submit" name="submit" value="送信">
+      <!-- FORM end -->
+</body>
+
+</html>
